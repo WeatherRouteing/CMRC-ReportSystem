4e649ae48fc7b3946cfd3571494c65e5e86cb4ac
WeaterRouteing <haozhangsmu@gmail.com>
2021-08-11 22:28:38 +0800

    1
    1

--- MysqlFunction/connectMysql.py
+++
@@ -1,4 +0,0 @@
-import pymysql
-def MysqlConnectFun(TableName):
-    db_connect = pymysql.connect(host='10.28.16.202', port=3306, user='root', passwd='daohang507', db='cmrcreportsystem',
-                             charset='utf8')
